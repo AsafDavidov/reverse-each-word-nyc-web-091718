b13c6814fc6c63439fb2ba9ed011423aa7cb6817
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 20:41:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   split_arr = str.split(" ")
   rev_string = ""
   split_arr.collect do |word| 
-    rev_string = rev_string + "#{word.reverse} "
+    rev_string = rev_string + "#{word.reverse}"
   end
   rev_string
 end  
